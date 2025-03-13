83aa6207579e615f1463a05c8abce697da3a30aa
Krishnanand <krishnanandunnikrishnan@gmail.com>
2025-03-13 23:50:10 +0530

    feat: private to public conversion and integration

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,14 @@
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import {toHex} from "ethereum-cryptography/utils.js";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance,privateKey,setPrivateKey }) { 
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value;
+
+    setPrivateKey (privateKey);
+    const address=secp256k1.getPublicKey(privateKey);
+    setAddress(toHex(address))
     if (address) {
       const {
         data: { balance },
@@ -19,9 +24,13 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type in a private key" value={privateKey} onChange={onChange}></input>
       </label>
+
+      <div>
+        Address: {address}
+      </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
